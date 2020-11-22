3526856b8e4ca6010af3709b50c21e6e67b85393
deybacsi <deyprof@gmail.com>
2020-11-22 15:16:36 +0100

    Forked workers are echoing the responses

--- http.c
+++ http.c
@@ -9,12 +9,16 @@
 #include "http.h"
 
 // server main configuration 
-const int   PORT=8080;                              // listening port
-const int   MAXCONNS=10;                            // max no of connections
+const int   PORT=8080,                              // listening port
+            MAXCONNS=10;                            // max no of connections
 
+/*
+    Initialize & start the server on specified port
+    Setup forking of new responder workers
+*/
 
 int startServer() {
-    fprintf(stdout, "Server starting on port %i...\n",PORT);
+    fprintf(stdout, "Server starting on port %i...\n", PORT);
 
     int listener,       // file descriptor for listening
         request;        // resp fd
@@ -24,10 +28,8 @@ int startServer() {
     bzero( &myAddr, sizeof(myAddr));                // clear the variable
 
     myAddr.sin_family = AF_INET;                    // addressing scheme IP
-    myAddr.sin_addr.s_addr = htons(INADDR_ANY);     // connect from any IP address
+    myAddr.sin_addr.s_addr = htons(INADDR_ANY);     // connections accepted from any IP address
     myAddr.sin_port = htons(PORT);                  // listen on port
-
-    
     listener = socket(AF_INET, SOCK_STREAM, 0);     // setup listener
     bind(listener, (struct sockaddr *) &myAddr, sizeof(myAddr));    // binding listener
     listen(listener, MAXCONNS);                     // start listening on port
@@ -39,14 +41,19 @@ int startServer() {
 
     while(1)
     {
-        request = accept(listener, (struct sockaddr*) NULL, NULL);
+        request = accept(listener, (struct sockaddr*) NULL, NULL);  // wait for incoming connections
 
-        forkId=fork();
+        forkId=fork();                                              // if input received, try to fork a new instance
         switch (forkId) {
-
-            case 0:                                                 // if forking was successful, this will be the worker thread
+            case 0:                                                 // if forking was successful, this will be the new worker thread
                 fprintf(stdout, "Worker forked successfully\n");
+                
+                bzero( str, 100);
+                read(request,str,100);
+                fprintf(stdout, "Echo:\n%s", str);
+                write(request, str, strlen(str)+1);
 
+                fprintf(stdout, "Worker exiting\n");
                 exit(0);
                 break;
             case -1:                                                // if something goes wrong print an error message
@@ -57,14 +64,9 @@ int startServer() {
                 break;
         }
 
-        bzero( str, 100);
- 
-        read(request,str,100);
- 
-        //printf("Received:\n %s",str);
- 
-        write(request, str, strlen(str)+1);
+
  
     }
 }
 
+ 
